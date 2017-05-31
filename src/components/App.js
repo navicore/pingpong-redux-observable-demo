c2786dd68be58abd11d4e72573392d3817c7546a
Ed Sweeney <ed@table.co>
2017-05-31 10:57:24 -0700

    styled components

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,49 @@
-import React, { Component } from "react";
+import React from "react";
+import logo from "../logo.svg";
 import { connect } from "react-redux";
 import { ping } from "../actions";
 import "rxjs";
+import styled from "styled-components";
+import PropTypes from "prop-types";
 
-let App = ({ isPinging, ping }) => (
-  <div>
-    <h1>is pinging: {isPinging.toString()}</h1>
-    <button onClick={ping}>Start PING</button>
-  </div>
+const Outer = styled.div`
+  text-align: center;
+`;
+
+const Header = styled.div`
+  background-color: #222;
+  height: 150px;
+  padding: 20px;
+  color: white;
+  > h2 {
+    color: red;
+  }
+`;
+
+const Button = styled.button`
+  background-color: grey;
+  height: 30px;
+  padding: 10px;
+  color: yellow;
+`;
+
+const Logo = styled.img`
+  animation: App-logo-spin infinite 20s linear;
+  height: 100px;
+`;
+
+const App = ({ isPinging, ping }) => (
+  <Outer>
+    <Header>
+      <Logo src={logo} alt="logo" />
+      <h2>is pinging: {isPinging.toString()}</h2>
+    </Header>
+    <Button onClick={ping}>Start PING</Button>
+  </Outer>
 );
+App.propTypes = {
+  isPinging: PropTypes.bool.isRequired,
+  ping: PropTypes.func.isRequired
+};
 
-App = connect(({ isPinging }) => ({ isPinging }), { ping })(App);
-
-export default App;
+export default connect(({ isPinging }) => ({ isPinging }), { ping })(App);
